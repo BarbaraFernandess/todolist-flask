56a1dcf6597c5b6c60c6a19c3a21e6be2e0db3b4
Barbara Fernandes <barbarafernandes61@gmail.com>
2024-06-11 16:59:33 -0300

    adding new funcionalities to the application

--- app.py
+++ app.py
@@ -2,15 +2,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-tasks = []
+tasksList = []
 # localhost:5000
 # localhost:5000/sobre
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == "POST":
-        if request.form.get("task"):
-            tasks.append(request.form.get("task"))
+        if request.form.get("task") != "":
+            tasksList.append(request.form.get("task"))
     return render_template(
         "index.html",
-        tasks=tasks
+        tasksList=tasksList
+    )
+    
+@app.route('/remove/<task>', methods=['POST', 'GET'])
+def remove(task):
+    if task in tasksList:
+        tasksList.remove(task)
+    return render_template(
+        "index.html",
+        tasksList=tasksList
+    )
+    
+@app.route('/clean')
+def clean():
+    tasksList.clear()
+    return render_template(
+        "index.html",
+        tasksList=tasksList
     )
